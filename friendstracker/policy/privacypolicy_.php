ce7d05511e1e364ad3c03f55ffc925682fbcbbfc
djmacross <djmacross@gmail.com>
2018-05-25 06:39:01 -0500

    GDPR

--- friendstracker/policy/privacypolicy_.php
+++ friendstracker/policy/privacypolicy_.php
@@ -11,6 +11,8 @@
 		font-size: 16px;
 		line-height: 30px;
 		font-weight: 200;
+		background-color: #175569;
+		color: white;
 	}
 	
 	h4 {		
@@ -31,24 +33,24 @@
 <body>
 <div class="section">
     <div class="container small">
-        
-         <div class="section-header">
-           <h4><center>
-           Who deleted me on Facebook?
-           </center></h4>
-        </div>  
 
-      <div class="section-header">
+        <div class="section-header">
+            <h4><center>
+            Who delete me on Facebook?
+            </center></h4>
+        </div>
+
+        <div class="section-header">
             <h5><br>
-              Privacy Policy</h5>
-        <p>Last Updated September 30, 2017</p>
+                Privacy Policy</h5>
+            <p>Last Updated May 25, 2018</p>
             <p>By installing the mobile application linking to this privacy policy (the &ldquo;Privacy Policy&rdquo; and &ldquo;Affiliated Application&rdquo;, accordingly) as portrayed on the License Agreement prompting on such Affiliated Application, you are agreeing to comply with, and be bound by, the following Privacy Policy, and other supplemental terms and conditions or documents that may be published from time to time. Please review the Privacy Policy carefully.</p>
-        </div>            
+        </div>
 
         <h4>General</h4>
         <p>Facebook® is a registered trademark of Facebook Inc., 1601 S California Ave, Palo Alto, CA 94304, USA (Facebook). All rights reserved.</p>
 
-        <p>Who deleted me on Facebook? is an independent tool that simply keeps track of your Facebook friends list changes. Your current friends list is analyzed and compared with the previous saved one (if any). In this way you can find who has deleted you from Facebook.</p>
+        <p>Who unfriended me? is an independent tool that simply keeps track of your Facebook friends list changes. Your current friends list is analyzed and compared with the previous saved one (if any). In this way you can find who has deleted you from Facebook.</p>
 
         <h4>Data Privacy Policy</h4>
 
@@ -59,37 +61,52 @@
 
         <p>
             For your information:      </p>
-      <ul>
-        <li>Only names and profile ids of your Facebook contacts are stored in the device.</li>
-                <li>This app uses this information only to compare your friends list with the previous one (if any) and to show you if anyone unfriended you.</li>
-                <li>Your data is store in your device, we don't use external servers.</li>
-                <li>Your friends list data will never be shared with third parties.</li>
-                <li>This app  does not alter or modify your list of friends.</li>
-                <li>Simply uninstall the app to remove your data.</li>
-      </ul>
+        <ul>
+            <li>Only names and profile ids of your Facebook contacts are stored in the device.</li>
+            <li>This app uses this information only to compare your friends list with the previous one (if any) and to show you if anyone unfriended you.</li>
+            <li>Your data is store in your device, we don't use external servers.</li>
+            <li>Your friends list data will never be shared with third parties.</li>
+            <li>This app  does not alter or modify your list of friends.</li>
+            <li>Simply uninstall the app to remove your data.</li>
+        </ul>
         </p>
         <p>We may automatically collect certain information from your device, including an Android, Apple iOS, or other ID, device make and model, mobile web browser type and version, IP address, MAC address, the device&rsquo;s operating system&rsquo;s make and version, locale information, MCC (Mobile Country Code) information, the mobile application name, a list of mobile applications installed on your device and other technical data about your device.</p>
-       
+        <p>&nbsp;</p>
 
-      <h4>How We Use Information</h4>
-      <p>Any information that you submit to us remains your property. However, we may use it for the purposes described herein.</p>
-      <p>Service. We reserve the right to use the information (including Personal Information) we collect, in order to administer, operate, and continuously improve our Service. We may also use the information to monitor and analyze trends, usage and activities in connection with our Service.</p>
-      <p>Advertisements. We reserve the right to use the Personal Information (and/or any other applicable information) we collect to create, market and distribute anonymized audience segments to third parties. We also allow third-party companies to serve ads and collect certain anonymous information when you visit our app. These companies may use anonymous information such as your Google Advertising ID, your device type and version, browsing activity, location and other technical data relating to your device, in order to provide advertisements.</p>
 
-           
-      
+        <h4>How We Use Information</h4>
+        <p>Any information that you submit to us remains your property. However, we may use it for the purposes described herein.</p>
+        <p>Service. We reserve the right to use the information (including Personal Information) we collect, in order to administer, operate, and continuously improve our Service. We may also use the information to monitor and analyze trends, usage and activities in connection with our Service.</p>
+        <p>Advertisements. We reserve the right to use the Personal Information (and/or any other applicable information) we collect to create, market and distribute anonymized audience segments to third parties. We also allow third-party companies to serve ads and collect certain anonymous information when you visit our app. These companies may use anonymous information such as your Google Advertising ID, your device type and version, browsing activity, location and other technical data relating to your device, in order to provide advertisements.</p>
+        <p>We may also use the information we collect for the following purposes:<br>
+         <ul>
+          <li>To find out the reason why the error occurred or application crashed and improve the quality of the application.</li>
+          <li>To help us develop new services.</li>
+          </ul>
+        We do not collect or disclose any sensitive personal information (e.g. information related to racial or ethnic origin, political opinions, religion or other beliefs, health, sexual orientations).
+		
+		<h4>Specific Provisions for EU Individuals</h4>
+		<p>If you are located in the European Union, the following provisions apply to you.
+On certain occasions, we process your personal data when it is necessary for the performance of a contract to which you are a party, such as to provide services to you. We may also process your personal data to respond to your inquiries concerning our products and services.
+On other occasions, we process your personal data where required by law. We may also process your personal data if necessary to protect your interests or the interests of a third party.
+Additionally, we process your personal data when necessary to do so for fraud prevention and improving our services, where these interests are not overridden by your data protection rights.
+If the processing of personal data is necessary and there is no other lawful basis for such processing, we will generally ensure that consent has been obtained from you. You have the right to withdraw your consent to processing of personal data at any time.</p>
 
-      <h4>Changes in this Privacy Statement</h4>
+	<h4>Children</h4>
+<p>We do not knowingly collect personal information from children under the age of 16 through the services.</p>
+
+
+        <h4>Changes in this Privacy Statement</h4>
         <p>We reserve the right to modify the Privacy Policy at any time, so please review it frequently. If we decide to change the Privacy Policy, we will post those changes to the Privacy Policy and update the &ldquo;Last Updated&rdquo; date above.  Your continued use of the Service following notice of changes to the Privacy Policy will constitute your acceptance of any changes. Provided that, we will not change the way we use your Personal Information without receiving your consent.      </p>
-      <h4>Contact</h4>
+        <h4>Contact</h4>
 
-        <p>You can contact us at <a href="mailto:support@fbfriendstracker.com">contacto@impactobtl.com</a></p>
+        <p>You can contact us at <a href="mailto:contacto@impactobtl.com">contacto@impactobtl.com</a></p>
 
         <p>&nbsp;</p>
 
-         
-    
-    </div>            
+
+
+    </div>
 </div>
 </body>
 </html>
